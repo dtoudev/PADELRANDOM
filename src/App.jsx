829e570e4a59e2da816e994341ac7dc52c232817
Diego Touriño <diegotourinogarcia@gmail.com>
2024-07-28 21:12:18 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import FormNames from "./components/FormNames"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <>
       <FormNames />
-      <SpeedInsights />
     </>
   )
 }
